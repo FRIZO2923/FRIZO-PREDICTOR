81d5f9af78eafba1e83d7ee63b7d01552ca5de62
FRIZO2923 <farhan89809488@gmail.com>
2025-04-06 21:27:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ if st.session_state.show_referral_message:
             <div style="border: 2px solid {color}; padding: 15px; border-radius: 12px; background-color: #f9f9f9; text-align: center; font-size: 18px; animation: blinker 1.5s linear infinite;">
                 🤑 <strong>Get ₹100 Cashback</strong> on ₹300 Recharge!<br>
                 👉 Create a new account using our referral link for best prediction results.<br><br>
-                🔗 <a href="https://your-referral-link.com" target="_blank" style="text-decoration: none; color: {color}; font-weight: bold;">Click Here to Register Now</a>
+                🔗 <a href="https://www.bigdaddygame.net//#/register?invitationCode=Narn6464148" target="_blank" style="text-decoration: none; color: {color}; font-weight: bold;">Click Here to Register Now</a>
             </div>
             <style>
                 @keyframes blinker {{
